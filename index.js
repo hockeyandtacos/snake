c1a51ed01d7062d1ed91a6520aae37896efb337a
Jeff Rosen <arirosen224@gmail.com>
2022-05-25 17:35:51 -0500

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + './public/css'))
+app.use('/js', express.static(__dirname + './public/js'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index', 
+    {
+        title: 'Snake Game',
+    }
+    )
+})
+
+app.listen(prosses.env.PORT || port, () => console.log(`availibe at http://localhost:${port}`))
